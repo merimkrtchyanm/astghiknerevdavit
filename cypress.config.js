517f107dc0a5f8529aa7f4d9caf62e50d04d61c2
Sona-SM <smargaryan88@gmail.com>
2023-05-08 13:55:08 +0400

    Main branch

--- cypress.config.js
+++ cypress.config.js
@@ -10,3 +10,21 @@ module.exports = defineConfig({
     }
   },
 });
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
+      const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
+      const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+
+      // await here
+      await require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin(on, config)
+
+      on('file:preprocessor',   createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+      return config;
+    }
+  },
+});
